e2905ab1015b626afa2fa35af427d22eaaf79a96
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-07-17 16:03:52 +0530

    changes

---
+++ server/config/constants.js
@@ -0,0 +1,7 @@
+/** Shared operational defaults for the PulseGrid demo runtime. */
+export const HTTP_PORT = 3001;
+export const JSON_BODY_LIMIT = '1mb';
+export const SIMULATION_REFRESH_INTERVAL_MS = 30_000;
+export const RECOMMENDATION_REFRESH_INTERVAL_MS = 60_000;
+export const SESSION_CLEANUP_INTERVAL_MS = 300_000;
+export const GRACEFUL_SHUTDOWN_TIMEOUT_MS = 5_000;
